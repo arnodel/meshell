d24ae73acd8f3c445f2f0f98b373532da9e718d4
Arnaud Delobelle <arnodel@gmail.com>
2021-10-23 22:58:51 +0100

    Remove old builtins code

--- command.go
+++ command.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"os"
 	"os/exec"
-	"strconv"
 )
 
 type StdStreams struct {
@@ -76,37 +75,6 @@ func (d *ExecCmdDef) Command(sh *Shell, std StdStreams) (Command, error) {
 	var err error
 	if f := builtins[cmdName]; f != nil {
 		return f(sh, std, args)
-	}
-	switch cmdName {
-	case "cd":
-		dir := ""
-		switch len(args) {
-		case 0:
-			dir, err = os.UserHomeDir()
-		case 1:
-			dir = args[0]
-		default:
-			err = errors.New("cd: wrong number of arguments")
-		}
-		if err != nil {
-			return nil, err
-		}
-		return &Cd{dir: dir, shell: sh}, nil
-	case "exit":
-		var code int64
-		switch len(args) {
-		case 0:
-			// default exit code
-		case 1:
-			codeStr := args[0]
-			code, err = strconv.ParseInt(codeStr, 10, 64)
-			if err != nil {
-				return nil, err
-			}
-		default:
-			return nil, errors.New("exit: wrong number of arguments")
-		}
-		return &Exit{code: int(code), shell: sh}, nil
 	}
 	cmd := exec.Command(parts[0], parts[1:]...)
 	dir, err := sh.GetCwd()
