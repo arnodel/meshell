995fe08699b9c09d7ad53f902fb041af039bb474
Arnaud Delobelle <arnodel@gmail.com>
2021-10-31 21:43:13 +0000

    Rudimentary script execution (stdin or by file name)

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -11,11 +13,91 @@ import (
 )
 
 func main() {
+	os.Exit(run())
+}
+
+func run() int {
 	debug := os.Getenv("DEBUG") != ""
 	var parseOpts []grammar.ParseOption
 	if debug {
 		parseOpts = append(parseOpts, grammar.WithDefaultLogger)
 	}
+	flag.Parse()
+	var (
+		filename string
+		script   []byte
+		err      error
+		args     []string
+	)
+	switch flag.NArg() {
+	case 0:
+		filename = "<stdin>"
+		if isaTTY(os.Stdin) {
+			return repl(debug, parseOpts)
+		}
+		args = []string{"meshell"}
+		script, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			fatal("Error reading <stdin>: %s", err)
+		}
+	default:
+		filename = flag.Arg(0)
+		script, err = ioutil.ReadFile(filename)
+		if err != nil {
+			return fatal("Error reading '%s': %s", filename, err)
+		}
+		args = flag.Args()
+	}
+	tokenStream, err := tokeniseCommand(string(script))
+	if err != nil {
+		return fatal("error parsing %s: %s", filename, err)
+	}
+	var line Line
+	parseErr := grammar.Parse(&line, tokenStream, parseOpts...)
+	if debug {
+		tokenStream.Dump(os.Stdout)
+	}
+	if parseErr != nil {
+		return fatal("error parsing %s: %s", filename, parseErr)
+	}
+	if line.CmdList == nil {
+		return 0
+	}
+	cmdDef, err := line.CmdList.GetCommand()
+	if err != nil {
+		return fatal("error interpreting %s: %s", filename, err)
+	}
+	cwd, _ := os.Getwd()
+	shell := NewShell(args[0], args[1:], cwd)
+	job, err := cmdDef.StartJob(shell, StdStreams{
+		In:  os.Stdin,
+		Out: os.Stdout,
+		Err: os.Stderr,
+	})
+	if err == nil {
+		res := job.Wait()
+		if !res.Success() {
+			err = res
+		}
+	}
+	if err != nil {
+		return fatal("error running %s: %s", filename, err)
+	}
+	return 0
+}
+
+func isaTTY(f *os.File) bool {
+	fi, _ := f.Stat()
+	return fi.Mode()&os.ModeCharDevice != 0
+}
+
+func fatal(tpl string, args ...interface{}) int {
+	fmt.Fprintf(os.Stderr, tpl, args...)
+	return 1
+}
+
+func repl(debug bool, parseOpts []grammar.ParseOption) int {
+
 	linr := liner.NewLiner()
 	defer linr.Close()
 	linr.SetCtrlCAborts(true)
@@ -26,7 +108,7 @@ outerLoop:
 		line, err := linr.Prompt(fmt.Sprintf("%s$ ", shell.GetCwd()))
 		if err == io.EOF {
 			fmt.Fprintln(os.Stdout, "\nBye!")
-			return
+			return 0
 		} else if err != nil {
 			fmt.Println(err)
 			continue
@@ -74,7 +156,7 @@ outerLoop:
 			} else if parseErr.Token == grammar.EOF {
 				more, err := linr.Prompt("> ")
 				if err == io.EOF {
-					return
+					return 0
 				} else if err != nil {
 					panic(err)
 				}
